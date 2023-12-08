2c7ad8ab6e463eb23e09d0af781b6dbb14f03b38
TerraCampos <gustav0.silva@protonmail.com>
2023-12-08 09:47:35 -0300

    Correção método soluonarTorreHanoi

--- src/main/java/Hanoi.java
+++ src/main/java/Hanoi.java
@@ -1,12 +1,10 @@
 public class Hanoi {
     public int solucionarTorreHanoi(int qtdDiscos) {
-        if (qtdDiscos <= 0) {
+        if (qtdDiscos < 0) {
             return 0;
         }
         else {
             return (int) Math.pow(2, (double) qtdDiscos) - 1;
         }
-
-
     }
 }
